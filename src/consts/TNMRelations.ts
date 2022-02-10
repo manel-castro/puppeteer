dbf191dc4926d5ceba10243b293f4f06c7f394ef
Manel Castro <manel.castro39@gmail.com>
2022-02-10 10:23:02 +0100

    pathologicalStage: adding matrix

--- src/consts/TNMRelations.ts
+++ src/consts/TNMRelations.ts
@@ -1,4 +1,4 @@
-type TNMRelation = {
+type cTNMRelation = {
   result: cTNMType;
   T: cTtype;
   N: cNtype;
@@ -8,7 +8,7 @@ export type cTNMType = "I" | "II" | "IIa" | "IIb" | "III" | "IVa" | "IVb";
 export type cTtype = "1" | "2" | "3" | "4a" | "4b";
 export type cNtype = "0" | "1" | "2" | "3" | "+" | "-";
 
-export const cTNMRelationsAdenocarcinoma: TNMRelation[] = [
+export const cTNMRelationsAdenocarcinoma: cTNMRelation[] = [
   {
     result: "I",
     T: "1",
@@ -61,7 +61,7 @@ export const cTNMRelationsAdenocarcinoma: TNMRelation[] = [
   },
 ];
 
-export const cTNMRelationsSquamousCarcinoma: TNMRelation[] = [
+export const cTNMRelationsSquamousCarcinoma: cTNMRelation[] = [
   {
     result: "I",
     T: "1",
@@ -112,4 +112,72 @@ export const cTNMRelationsSquamousCarcinoma: TNMRelation[] = [
     T: "4b",
     N: "+",
   },
+];
+
+type pTNMRelation = {
+  result: pTNMType;
+  T: pTtype;
+  N: pNtype;
+  differentiationGrade: differentiationGradeType;
+};
+
+export type pTNMType =
+  | "Ia"
+  | "Ib"
+  | "Ic"
+  | "IIa"
+  | "IIb"
+  | "IIIa"
+  | "IIIb"
+  | "IVa"
+  | "IVb";
+export type pTtype = "1a" | "1b" | "2" | "3" | "4a" | "4b";
+export type pNtype = "0" | "1" | "2" | "3" | "+" | "-";
+export type differentiationGradeType = "G1" | "G2" | "G3";
+
+export const pTNMRelationsAdenocarcinoma: pTNMRelation[] = [
+  {
+    result: "Ia",
+    differentiationGrade: "G1",
+    T: "1a",
+    N: "0",
+  },
+  {
+    result: "Ib",
+    differentiationGrade: "G2",
+    T: "1a",
+    N: "0",
+  },
+  {
+    result: "Ic",
+    differentiationGrade: "G3",
+    T: "1a",
+    N: "0",
+  },
+  {
+    result: "Ib",
+    differentiationGrade: "G1" || "G2",
+    T: "1b",
+    N: "0",
+  },
+  {
+    result: "Ic",
+    differentiationGrade: "G3",
+    T: "1b",
+    N: "0",
+  },
+
+  // left on COLUMN N0 T1b
+  // {
+  //   result: "Ib",
+  //   differentiationGrade: "G1" || "G2",
+  //   T: "1b",
+  //   N: "0",
+  // },
+  // {
+  //   result: "Ic",
+  //   differentiationGrade: "G3",
+  //   T: "1b",
+  //   N: "0",
+  // },
 ];
